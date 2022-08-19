a3c732fcdeac5b418bc6fd4fd46de4bb64071e9b
难寻芳踪 <84896150@qq.com>
2022-08-19 21:46:59 +0800

    Update store.go

--- core/store.go
+++ core/store.go
@@ -4,12 +4,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/beego/beego/v2/adapter/logs"
-	"github.com/boltdb/bolt"
 	"reflect"
 	"runtime"
 	"strconv"
 	"strings"
+
+	"github.com/boltdb/bolt"
 )
 
 var sillyGirl Bucket = NewBucket("sillyGirl")
@@ -17,34 +17,15 @@ var db *bolt.DB
 
 type Bucket string
 
-var Buckets = []Bucket{}
-
-func initStore() {
-	var err error
-	if runtime.GOOS == "darwin" {
-		db, err = bolt.Open("./sillyGirl.cache", 0600, nil)
-		logs.Info("打开数据库", `db=`, db, `err=`, err)
-	} else {
-		db, err = bolt.Open(dataHome+"/sillyGirl.cache", 0600, nil)
-		logs.Info("打开数据库", `db=`, db, `err=`, err)
-	}
-	if err != nil
-		panic(err)
-	}
-}
-
 func (b Bucket) String() string {
-	logs.Info(string(b))
 	return string(b)
 }
 
-
+var Buckets = []Bucket{}
 
 func NewBucket(name string) Bucket {
-	logs.Info("NewBucket", `name=`, name)
 	b := Bucket(name)
 	Buckets = append(Buckets, b)
-	logs.Info("NewBucket-re", `Buckets=`, Buckets, `b=`, b)
 	return b
 }
 
@@ -52,37 +33,37 @@ func GetDB() *bolt.DB {
 	return db
 }
 
+func initStore() {
+	var err error
+	if runtime.GOOS == "darwin" {
+		db, err = bolt.Open("./sillyGirl.cache", 0600, nil)
+	} else {
+		db, err = bolt.Open(dataHome+"/sillyGirl.cache", 0600, nil)
+	}
+	if err != nil {
+		panic(err)
+	}
+}
 
-
-func (bucket Bucket) Set(key interface{}, value interface{}) error {
-	logs.Info("Set", `table=`, bucket, `key=`, key, `value=`, value)
-	return db.Update(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucketIfNotExists([]byte(bucket))
-		if err != nil {
-			return err
+func (bucket Bucket) Set(key interface{}, value interface{}) {
+	db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		if b == nil {
+			b, _ = tx.CreateBucket([]byte(bucket))
 		}
 		k := fmt.Sprint(key)
 		if _, ok := value.([]byte); !ok {
 			v := fmt.Sprint(value)
 			if v == "" {
-				if err := b.Delete([]byte(k)); err != nil {
-					return err
-				}
+				b.Delete([]byte(k))
 			} else {
-				if err := b.Put([]byte(k), []byte(v)); err != nil {
-					return err
-				}
+				b.Put([]byte(k), []byte(v))
 			}
 		} else {
 			if len(value.([]byte)) == 0 {
-				if err := b.Delete([]byte(k)); err != nil {
-					return err
-				}
-
+				b.Delete([]byte(k))
 			} else {
-				if err := b.Put([]byte(k), value.([]byte)); err != nil {
-					return err
-				}
+				b.Put([]byte(k), value.([]byte))
 			}
 		}
 		return nil
@@ -90,20 +71,14 @@ func (bucket Bucket) Set(key interface{}, value interface{}) error {
 }
 
 func (bucket Bucket) Push2Array(key, value string) {
-	logs.Info("Push2Array", `table=`, bucket, `key=`, key, `value=`, value)
-	bucket.Set(key, strings.Join(append(strings.Split(bucket.GetString(key), ","), value), ","))
+	bucket.Set(key, strings.Join(append(strings.Split(bucket.Get(key), ","), value), ","))
 }
 
 func (bucket Bucket) GetArray(key string) []string {
-	logs.Info("GetArray", `table=`, bucket, `key=`, key)
-	return strings.Split(bucket.GetString(key), ",")
+	return strings.Split(bucket.Get(key), ",")
 }
+
 func (bucket Bucket) Get(kv ...interface{}) string {
-	logs.Info("Get", `table=`, bucket, `kv=`, kv)
-	return bucket.GetString(kv)
-}
-func (bucket Bucket) GetString(kv ...interface{}) string {
-	logs.Info("GetString", `table=`, bucket, `kv=`, kv)
 	var key, value string
 	for i := range kv {
 		if i == 0 {
@@ -112,6 +87,7 @@ func (bucket Bucket) GetString(kv ...interface{}) string {
 			value = fmt.Sprint(kv[1])
 		}
 	}
+
 	db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(bucket))
 		if b == nil {
@@ -126,7 +102,6 @@ func (bucket Bucket) GetString(kv ...interface{}) string {
 }
 
 func (bucket Bucket) GetBytes(key string) []byte {
-	logs.Info("GetBytes", `table=`, bucket, `key=`, key)
 	var value []byte
 	db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(bucket))
@@ -142,7 +117,6 @@ func (bucket Bucket) GetBytes(key string) []byte {
 }
 
 func (bucket Bucket) GetInt(key interface{}, vs ...int) int {
-	logs.Info("GetInt", `table=`, bucket, `key=`, key, `vs=`, vs)
 	var value int
 	if len(vs) != 0 {
 		value = vs[0]
@@ -162,7 +136,6 @@ func (bucket Bucket) GetInt(key interface{}, vs ...int) int {
 }
 
 func (bucket Bucket) GetBool(key interface{}, vs ...bool) bool {
-	logs.Info("GetBool", `table=`, bucket, `key=`, key, `vs=`, vs)
 	var value bool
 	if len(vs) != 0 {
 		value = vs[0]
@@ -184,54 +157,37 @@ func (bucket Bucket) GetBool(key interface{}, vs ...bool) bool {
 }
 
 func (bucket Bucket) Foreach(f func(k, v []byte) error) {
-	logs.Info("Foreach", `table=`, bucket, `f=`, f)
-	var bs = [][][]byte{}
 	db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(bucket))
 		if b != nil {
-			err := b.ForEach(func(k, v []byte) error {
-				bs = append(bs, [][]byte{k, v})
-				return nil
-			})
-			if err != nil {
-				return err
-			}
+			b.ForEach(f)
 		}
 		return nil
 	})
-	for i := range bs {
-		f(bs[i][0], bs[i][1])
-	}
 }
 
 var Int = func(s interface{}) int {
-	logs.Info("Int", `s=`, s)
 	i, _ := strconv.Atoi(fmt.Sprint(s))
 	return i
 }
 
 var Int64 = func(s interface{}) int64 {
-	logs.Info("Int64", `s=`, s)
 	i, _ := strconv.Atoi(fmt.Sprint(s))
 	return int64(i)
 }
 
 func (bucket Bucket) Create(i interface{}) error {
-	logs.Info("Create", `i=`, i)
 	s := reflect.ValueOf(i).Elem()
 	id := s.FieldByName("ID")
 	sequence := s.FieldByName("Sequence")
 	return db.Update(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucketIfNotExists([]byte(bucket))
-		if err != nil {
-			return err
+		b := tx.Bucket([]byte(bucket))
+		if b == nil {
+			b, _ = tx.CreateBucket([]byte(bucket))
 		}
 		if _, ok := id.Interface().(int); ok {
 			key := id.Int()
-			sq, err := b.NextSequence()
-			if err != nil {
-				return err
-			}
+			sq, _ := b.NextSequence()
 			if key == 0 {
 				key = int64(sq)
 				id.SetInt(key)
@@ -246,10 +202,7 @@ func (bucket Bucket) Create(i interface{}) error {
 			return b.Put(itob(uint64(key)), buf)
 		} else {
 			key := id.String()
-			sq, err := b.NextSequence()
-			if err != nil {
-				return err
-			}
+			sq, _ := b.NextSequence()
 			if key == "" {
 				key = fmt.Sprint(sq)
 				id.SetString(key)
@@ -267,17 +220,15 @@ func (bucket Bucket) Create(i interface{}) error {
 }
 
 func itob(i uint64) []byte {
-	logs.Info("12334", `i=`, i)
 	return []byte(fmt.Sprint(i))
 }
 
 func (bucket Bucket) First(i interface{}) error {
-	logs.Info("First", `i=`, i)
 	var err error
 	s := reflect.ValueOf(i).Elem()
 	id := s.FieldByName("ID")
 	if v, ok := id.Interface().(int); ok {
-		err = db.View(func(tx *bolt.Tx) error {
+		db.View(func(tx *bolt.Tx) error {
 			b := tx.Bucket([]byte(bucket))
 			if b == nil {
 				err = errors.New("bucket not find")
@@ -288,10 +239,11 @@ func (bucket Bucket) First(i interface{}) error {
 				err = errors.New("record not find")
 				return nil
 			}
-			return json.Unmarshal(data, i)
+			json.Unmarshal(data, i)
+			return nil
 		})
 	} else {
-		err = db.View(func(tx *bolt.Tx) error {
+		db.View(func(tx *bolt.Tx) error {
 			b := tx.Bucket([]byte(bucket))
 			if b == nil {
 				err = errors.New("bucket not find")
@@ -302,8 +254,36 @@ func (bucket Bucket) First(i interface{}) error {
 				err = errors.New("record not find")
 				return nil
 			}
-			return json.Unmarshal(data, i)
+			json.Unmarshal(data, i)
+			return nil
 		})
 	}
+
 	return err
 }
+
+func (bucket Bucket) Find(is []interface{}) error {
+	var err error
+	// is = append(is, interface{})
+	// reflect.ValueOf(is).
+	// id := reflect.ValueOf(i).Elem().FieldByName("ID").Int()
+	// db.View(func(tx *bolt.Tx) error {
+	// 	b := tx.Bucket([]byte(bucket))
+	// 	if b == nil {
+	// 		err = errors.New("bucket not find")
+	// 		return nil
+	// 	}
+	// 	data := b.Get(itob(uint64(id)))
+	// 	if len(data) == 0 {
+	// 		err = errors.New("record not find")
+	// 		return nil
+	// 	}
+	// 	json.Unmarshal(data, i)
+	// 	return nil
+	// })
+	return err
+}
+
+// func (bucket Bucket) Find(o interface{}) {
+
+// }
